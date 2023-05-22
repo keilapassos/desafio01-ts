3d7800970b2e695325e128dafd17a7b80957c323
Keila Aparecida Rpdrogues Passos <keila.rodrigues.passos@gmail.com>
2023-05-22 18:46:32 -0300

    feat: instanciei um novo objeto de PremiumAccount e usei seu método deposit

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import { CompanyAccount } from "./class/CompanyAccount";
 import { PeopleAccount } from "./class/PeopleAccount";
+import { PremiumAccount } from "./class/PremiumAccount";
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1, "Nath", 10);
 // console.log(peopleAccount);
@@ -17,3 +18,7 @@ companyAccount.withdraw(10);
 companyAccount.withdraw(50);
 
 companyAccount.getLoan(500);
+
+const premiumAccount: PremiumAccount = new PremiumAccount("Keila", 3);
+premiumAccount.deposit(25);
+premiumAccount.deposit(100);
