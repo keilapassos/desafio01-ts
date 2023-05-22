d26c5f198471ce5a7c2129b97342d019603d79d6
Keila Aparecida Rpdrogues Passos <keila.rodrigues.passos@gmail.com>
2023-05-22 19:14:25 -0300

    adicionei um novo depósito a conta premiumAccount

--- class/PremiumAccount.ts
+++ class/PremiumAccount.ts
@@ -7,13 +7,15 @@ export class PremiumAccount extends DioAccount {
 
   deposit = (value: number): void => {
     const aditional: number = 10;
+    const totalDeposit: number = value + aditional;
 
-    this.balance += value + aditional;
+    this.setBalance(totalDeposit);
+
     console.log(`---------- DEPÓSITO COM ACRÉSCIMO ----------`);
     console.log(
-      `Voce depositou R$ ${value} + e teve acréscimo de R$ ${aditional}`
+      `Voce depositou R$ ${value} e teve acréscimo de R$ ${aditional}. Total depositado: R$ ${totalDeposit}`
     );
-    console.log(`Saldo em conta atualizado: R$ ${this.balance}`);
+    console.log(`Saldo em conta atualizado: R$ ${this.getBalance()}`);
     console.log();
   };
 }
